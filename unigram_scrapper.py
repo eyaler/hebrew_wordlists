8892f4f335380b5dd93d08a7b941614797c40113
eyaler <eyalgruss@gmail.com>
2021-07-15 14:44:44 +0300

    fixed small bug with line splitting (not affectind cc100)

--- unigram_scrapper.py
+++ unigram_scrapper.py
@@ -27,11 +27,9 @@ lines_above_max_chars_allowed = 0
 corpus = file.rsplit('/', 1)[-1].split('_')[0]
 rejected_lines_file = 'd:/data/text/heb/%s_rejected_lines.txt'%corpus
 rejected_words_file = '%s_rejected_words.csv'%corpus
-with open(file, encoding='utf8') as f, open(rejected_lines_file, 'w', encoding='utf8') as fbad:
-    for line in f:
-        i+=1
-        if i%1000000==0:
-            print(i)
+with open(file, 'rb') as f, open(rejected_lines_file, 'wb') as fbad: # use binary mode for correct line splitting (https://stackoverflow.com/questions/56781896/wc-l-and-python-line-count-differ)
+    for bline in f:
+        line = bline.decode('utf8')
         line_words = heb_tokenizer.get_words(line)
         if len(line)>max_line_chars:
             max_line_chars = len(line)
@@ -47,9 +45,12 @@ with open(file, encoding='utf8') as f, open(rejected_lines_file, 'w', encoding='
                 lines_above_max_chars_allowed += 1
             skip_lines += 1
             skip_words.update(line_words)
-            fbad.write(line)
+            fbad.write(bline)
         else:
             words.update(line_words)
+        i+=1
+        if i%1000000==0:
+            print(i)
 
 sum_cnt = 0
 with open('%s.csv'%corpus, 'w', encoding='utf8') as f:
@@ -96,13 +97,13 @@ cnt_counter: [('1', 1954447), ('10+', 1060415), ('2', 600708), ('3', 297955), ('
 max_line_words_counter: [('את', 100), ('של', 55), ('לא', 40), ('או', 24), ('מה', 17), ('עם', 16), ('על', 15), ('בין', 13), ('ללא', 12), ('לך', 11)]
 corpus=cc100 total_lines=207542919 total_words=3140829404 used_lines=207255007 (1.00) used_words=3134313276 (1.00) unique_words=4551907 skipped_lines=287912 (0.00) skipped_words=6516128 (0.00) lines_above_max_chars_allowed=1722 max_line_chars=15608 (i=90632729) max_line_words=2678 (i=90632729)
 
-oscar skipping 'גוגל מפות' and len>4000:
+oscar skipping 'גוגל מפות' and len>4000 (lines/indices are off by a few due to former line splitting bug):
 len_counter: [('10+', 20641172), ('0', 69689), ('9', 36517), ('8', 31133), ('7', 26444), ('6', 20705), ('5', 11660), ('4', 8038), ('2', 5758), ('1', 3420), ('3', 3380)]
 cnt_counter: [('1', 1357968), ('10+', 708344), ('2', 388616), ('3', 197632), ('4', 127616), ('5', 88030), ('6', 68057), ('7', 53840), ('8', 43818), ('9', 36641)]
 max_line_words_counter: [('קקי', 16780), ('אוזון', 16780), ('בולבולים', 12527)]
 corpus=oscar total_lines=20857916 total_words=948706943 used_lines=19483761 (0.93) used_words=874066233 (0.92) unique_words=3070562 skipped_lines=1374155 (0.07) skipped_words=74640710 (0.08) max_line_chars=490572 (7334090) max_line_words=46087 (17984717)
 
-oscar skipping 'גוגל מפות', '|' and len>4000:
+oscar skipping 'גוגל מפות', '|' and len>4000 (lines/indices are off by a few due to former line splitting bug):
 len_counter: [('10+', 20641172), ('0', 69689), ('9', 36517), ('8', 31133), ('7', 26444), ('6', 20705), ('5', 11660), ('4', 8038), ('2', 5758), ('1', 3420), ('3', 3380)]
 cnt_counter: [('1', 1353895), ('10+', 706412), ('2', 387259), ('3', 196966), ('4', 127201), ('5', 87697), ('6', 67831), ('7', 53780), ('8', 43636), ('9', 36564)]
 max_line_words_counter: [('קקי', 16780), ('אוזון', 16780), ('בולבולים', 12527)]
